264c8acadeae820f1c665433b203d840e91aa349
rbeelaprolu <reelaprolu@afmsagaftrafund.org>
2023-11-17 09:55:46 +0530

    First commit of Cards service

---
+++ src/test/java/com/kfarm/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfarm.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
